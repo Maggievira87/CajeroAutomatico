d0b17572d57f1227cb60a6b62245faafdd78b27c
Maggievira87 <margarethjvr87@gmail.com>
2025-11-26 16:06:11 -0500

    nuevo Cambio Margareth

--- scr/facturacion.java
+++ scr/facturacion.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class facturacion1 {
+public class facturacion {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
